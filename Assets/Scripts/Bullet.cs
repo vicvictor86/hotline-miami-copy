6f14a6f73afdb15f42581b50a430217bc2759868
Felipe.Osti <felipeosti@pecege.com>
2022-04-13 22:43:32 -0300

    Refactor: ajustar shotgun

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,8 +7,11 @@ public class Bullet : MonoBehaviour
     public GameObject hitEffect;
     void OnCollisionEnter2D(Collision2D collision)
     {
-        GameObject effect =  Instantiate(hitEffect, transform.position, Quaternion.identity);
-        Destroy(effect, 5f);
-        Destroy(gameObject);
+        if (collision.gameObject.tag != "Bullet")
+        {
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 2.5f);
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/Shotgun.cs
+++ Assets/Scripts/Shotgun.cs
@@ -3,31 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shotgun : MonoBehaviour, IGun
+public class Shotgun : Gun, IGun
 {
-    public GameObject BulletPrefab { get; set; }
-    public Transform FirePoint { get; set; }
-    public float BulletForce { get; set; }
-    
-    private void Awake()
+    public new void Shoot()
     {
-        Shooting shooting = GetComponent<Shooting>();
-        BulletPrefab = shooting.bulletPrefab;
-        FirePoint = shooting.firePoint;
-        BulletForce = shooting.bulletForce;
+        int angle = 7;
+        CreateBullet(0);
+        CreateBullet(angle);
+        CreateBullet(-angle);
     }
-    
-    public void Shoot()
+
+    private void CreateBullet(int angle)
     {
-        Debug.Log(FirePoint.right);
-        GameObject bullet = Instantiate(BulletPrefab, FirePoint.position, FirePoint.rotation);
-        GameObject bullet2 = Instantiate(BulletPrefab, FirePoint.position, FirePoint.rotation * Quaternion.Euler(0,90,0));
-        GameObject bullet3 = Instantiate(BulletPrefab, FirePoint.position, FirePoint.rotation * Quaternion.Euler(0,-90,0));
+        Quaternion rotation = Quaternion.Euler(0, 0, angle);
+        GameObject bullet = Instantiate(BulletPrefab, FirePoint.position, rotation * FirePoint.rotation);
         var rb = bullet.GetComponent<Rigidbody2D>();
-        var rb2 = bullet2.GetComponent<Rigidbody2D>();
-        var rb3 = bullet3.GetComponent<Rigidbody2D>();
-        rb.AddForce(FirePoint.right * BulletForce, ForceMode2D.Impulse);
-        rb2.AddForce((FirePoint.right * new Vector2(1, (float) (Math.Sqrt(2)/2))) * BulletForce, ForceMode2D.Impulse);
-        rb3.AddForce((FirePoint.right * new Vector2(1, (float) -(Math.Sqrt(2)/2))) * BulletForce, ForceMode2D.Impulse);
+        rb.AddForce(rotation * FirePoint.right * BulletForce, ForceMode2D.Impulse);
     }
 }
